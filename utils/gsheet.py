ba4d1c84e946d5e950f04cd400aa7ed844f53266
1112krisha <99976382+1112krisha@users.noreply.github.com>
2025-06-21 16:56:14 +0530

    Update gsheet.py

--- utils/gsheet.py
+++ utils/gsheet.py
@@ -2,15 +2,16 @@ import pandas as pd
 import json
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
 def get_data_from_sheet():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
     creds_dict = json.loads(st.secrets["GOOGLE_CREDS"])
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-
-    import gspread
     client = gspread.authorize(creds)
-    sheet = client.open("Advait_Insights_Backend").sheet1
+
+    # Make sure both name and tab are correct
+    sheet = client.open("Advait_Insights_Backend").worksheet("Sheet1")
+
     data = sheet.get_all_records()
     return pd.DataFrame(data)
